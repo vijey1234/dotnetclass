3a4049c716cf273def6578e7d728666645214b91
LAPTOP-2N1DENML\vijey <vijeyendarkittu@gmail.com>
2021-03-09 18:11:20 -0500

    first commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Console.WriteLine("Hello World! "+i);
+        }
+    }
+}
---
+++ ConsoleAppVB/Program.vb
@@ -0,0 +1,8 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim i As Integer = 10
+        Console.WriteLine("Hello Worldvb " + i)
+    End Sub
+End Module
