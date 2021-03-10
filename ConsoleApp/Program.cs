42622aa4ecc22c6da1077e3b86361315fb98be4d
LAPTOP-2N1DENML\vijey <vijeyendarkittu@gmail.com>
2021-03-10 16:08:57 -0500

    Added a new function for addition

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,8 +6,31 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            int i = 10;
-            Console.WriteLine("Hello World! "+i);
+            //int i = 10;
+            //Console.WriteLine("Hello World! "+i);
+            TestAdd();
+
+        }
+
+        static void TestAdd()
+        {
+  
+            int a;
+            int b;
+            Console.WriteLine("Enter value of a: ");
+
+           a = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter value of b: ");
+
+            // Converted string to int 
+            b = Convert.ToInt32(Console.ReadLine());
+
+            
+                Console.WriteLine("addition of a and b is:" +(a+b));
+     
+
         }
     }
 }
