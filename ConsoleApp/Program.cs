b1d377606c15734a4751af88e4007743d94efc78
LAPTOP-H1RL0LRG\prasa <prasadjoshidev@gmail.com>
2021-03-10 18:57:21 -0500

    march_10

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,35 +1,58 @@
-﻿using System;
+﻿
+using System;
 
-namespace ConsoleApp
-{
-    class Program
+namespace method
     {
-        static void Main(string[] args)
+        class Program
         {
-            //int i = 10;
-            //Console.WriteLine("Hello World! "+i);
-            TestAdd();
+            static void Main(string[] args)
+            {
+                //int i = 10;
+                // Console.WriteLine("Hello World!");
 
+            }
+            static int TestAdd()
+            {
+                int a = 0;
+                //int b;
+                Console.WriteLine("Enter value for a: ");
+                Console.ReadLine();
+                Console.WriteLine("Enter value for b: ");
+                Console.ReadLine();
+                return a;
+            }
+            static string TestAdd(int x)
+            {
+                string a = "sum";
+                Console.WriteLine("addition of a and b: (a+b) ");
+                Console.ReadLine();
+                return a;
+
+            }
+            static string TestAdd(string x)
+            {
+                string a = "sum";
+                Console.WriteLine("addition of a and b: (a+b) ");
+                Console.ReadLine();
+                return a;
+            }
+            static string TestAdd(int x, int y)
+            {
+                string a = "sum";
+                Console.WriteLine("addition of a and b: (a+b) ");
+                Console.ReadLine();
+                return a;
+            }
+        }
+        class Program1
+        {
+            static string TestAdd(int x)
+            {
+                string a = "sum";
+                Console.WriteLine("addition of a and b: (a+b) ");
+                Console.ReadLine();
+                return a;
+            }
         }
 
-        static void TestAdd()
-        {
-  
-            int a;
-            int b;
-            Console.WriteLine("Enter value of a: ");
-
-           a = Convert.ToInt32(Console.ReadLine());
-
-
-            Console.WriteLine("Enter value of b: ");
-
-            b = Convert.ToInt32(Console.ReadLine());
-
-            
-                Console.WriteLine("addition of a and b is:" +(a+b));
-     
-
-        }
     }
-}
