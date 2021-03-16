364a97d6886bbaddbd15a11677c1b7f137098390
LAPTOP-H1RL0LRG\prasa <prasadjoshidev@gmail.com>
2021-03-16 19:20:17 -0400

    March_16

---
+++ MVC/App_Start/RouteConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Office",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Office", action = "Office", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Employee",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Employee", action = "Create", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Employee1",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Employee", action = "View", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MVC/Controllers/OfficeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class OfficeController : Controller
+    {
+        // GET: Office
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Office()
+        {
+            return View();
+        }
+    }
+}
